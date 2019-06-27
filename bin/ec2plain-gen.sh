37d6219935c4893646a43d4ca55605632a539f70
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-06-26 20:19:15 -0400

    working well for ec2plain-gen

--- bin/ec2plain-gen.sh
+++ bin/ec2plain-gen.sh
@@ -11,7 +11,7 @@ if [ "${VERBOSE}" == "1" ]; then echo SCRIPTFULLFLDRPATH=${SCRIPTFULLFLDRPATH}; 
 #____	if [ "${SCRIPTFLDR_RELATIVE}" != "." ]; then
 #____	fi
 
-.   ${SCRIPTFULLFLDRPATH}/cfngen-common.sh
+.   ${SCRIPTFULLFLDRPATH}/cfngen-common-pre.sh
 
 ###=============================================================
 ###@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -20,36 +20,66 @@ if [ "${VERBOSE}" == "1" ]; then echo SCRIPTFULLFLDRPATH=${SCRIPTFULLFLDRPATH}; 
 ### !!!!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!
 ### Make sure 'cd' command below comes __AFTER__ sourcing 'common.sh' (above)
 
-if [ -d ${JobSetName} ]; then
-	cd ${JobSetName}
-fi
-USERFLDR=`pwd`
-if [ "${VERBOSE}" == "1" ]; then echo ${USERFLDR}; fi
+CFNContext=ec2plain		### This defines which OTHER property files are loaded - - within 'cfngen-common-POST.sh'
+
+.   ${SCRIPTFULLFLDRPATH}/cfngen-common-post.sh
 
 ###=============================================================
 ###@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 ###=============================================================
 
-${SCRIPTFULLFLDRPATH}/AWS-AMI-list-by-Region.sh ${AWSRegion} | grep -v '.-.*=' > /tmp/$$  ### Weed out 'ap-northeast-1LinuxAMI=...' and keep only 'TokyoLimuxAMI='
-if [ -e /tmp/$$ ] && [ -s /tmp/$$ ]; then
-	. /tmp/$$
-	\rm /tmp/$$
+# ${SCRIPTFULLFLDRPATH}/AWS-AMI-list-by-Region.sh ${AWSRegion} | grep -v '.-.*=' > /tmp/$$  ### Weed out 'ap-northeast-1LinuxAMI=...' and keep only 'TokyoLimuxAMI='
+# if [ -e /tmp/$$ ] && [ -s /tmp/$$ ]; then
+# 	COUNT=`wc -l < /tmp/$$`
+# 	COUNT=`expr $COUNT + 0`  ### Convert 'string _output_ of wc-command' into integer
+# 	if [ $count -eq 1 ]; then
+# 		. /tmp/$$
+# 		\rm /tmp/$$
+# 		bAllOk = 1; ### <<--  bAllOk is ONLY defined here and NO WHERE else
+# 	fi
+# fi
+# if [ -z ${bAllOk+x} ]; then  ### if [ -z "$bAllOk" ]    <-- does NOT distinguish between 'unset bAllOk' & bAllOk=""
+# 	>&2 echo "ERROR!!!!!!! Failed to find a UNIQUE-SINGLE AMI, using: ${SCRIPTFULLFLDRPATH}/AWS-AMI-list-by-Region.sh ${AWSRegion} | grep -v '.-.*='"
+# 	exit 19
+# fi
+# if [ "${VERBOSE}" == "1" ]; then	echo AWSAMIID=${AWSLocation}LinuxAMI; fi
+
+AMIIDCache=${AWSCFNHOME}/config/inputs/AMZNLinux2_AMI_ID-${AWSLocation}.txt
+if [ -e ${AMIIDCache} ] && [ -s ${AMIIDCache} ]; then
+	AWSAMIID=`cat ${AMIIDCache}`
 else
-	>&2 echo "ERROR!!!!!!! No AMIs found using: ${SCRIPTFULLFLDRPATH}/AWS-AMI-list-by-Region.sh ${AWSRegion} | grep -v '.-.*='"
-	exit 19
+	### !!!! NOTE !!!!! ${EC2AMI_LookupKey} is set within the job-file ${JobSetName}/jobset-ec2plain.properties <<---------
+	### !! Note !! That value of the variable is determined by another script.       AWS/CFN/bin/AWS-AMI-list-by-Region.sh <AWSRegion>
+	###	!! Note !! In the output of that command .. Remove the VERSION#.DATE from the middle of the AMI-TYPE, to get the EC2AMI_LookupKey
+	echo "Querying AWS to figure out .. what the AMI-ID for ${EC2AMI_LookupKey} in the Location ${AWSLocation} is."
+	if [ "${VERBOSE}" == "1" ]; then echo aws ssm get-parameters --names "/aws/service/ami-amazon-linux-latest/${EC2AMI_LookupKey}" --region "${AWSRegion}" --profile ${AWSprofile} --output json; fi
+	aws ssm get-parameters --names "/aws/service/ami-amazon-linux-latest/${EC2AMI_LookupKey}" --region "${AWSRegion}" --profile ${AWSprofile} --output json > /tmp/o.json
+	asux yaml batch "useAsInput @/tmp/o.json ; yaml --read Parameters,0,Value --delimiter ," --no-quote -i /dev/null -o - > /tmp/$$
+	AWSAMIID=`cat /tmp/$$`
+	echo AWSAMIID=`cat /tmp/$$` > ${AMIIDCache}
+	\rm /tmp/o.json /tmp/$$
 fi
 
-if [ "${VERBOSE}" == "1" ]; then	echo AWSAMIID=${AWSLocation}LinuxAMI; fi
-
 ###------------------------------
+###???????????????????????????????????? In java make sure each and every REQUIRED CFN-Parameter below is defined before executing commands below.
 
-CFNfile=/tmp/ec2
+CFNfile=/tmp/${CFNContext}
 
-${ORGASUXHOME}/asux.js yaml batch @${AWSCFNHOME}/bin/AWSCFN-MyEC2plain-Create.ASUX-batch.txt -i /dev/null -o ${CFNfile}
-PARAMS=" ParameterKey=MyVPC,ParameterValue==${VPCID} ParameterKey=MyPublicSubnet1,ParameterValue==${DefaultPublicSubnet1} "
-PARAMS="${PARAMS} ParameterKey=MySSHSecurityGroup,ParameterValue==${MySSHSecurityGroup}  ParameterKey=MyIamInstanceProfiles,ParameterValue==${MyIamInstanceProfiles} "
-PARAMS="${PARAMS} ParameterKey=AWSAMIID,ParameterValue==${AWSAMIID} ParameterKey=MySSHKeyName,ParameterValue==${MySSHKeyName} "
-echo "aws cloudformation create-stack --stack-name --parameters ${PARAMS} " > ${CFNfile}.sh
+${ORGASUXHOME}/asux.js yaml batch @${AWSCFNHOME}/bin/AWSCFN-${CFNContext}-Create.ASUX-batch.txt -i /dev/null -o ${CFNfile}
+PARAMS=" ParameterKey=MyPublicSubnet1,ParameterValue=${DefaultPublicSubnet1} "
+PARAMS="${PARAMS} ParameterKey=MySSHSecurityGroup,ParameterValue=${MySSHSecurityGroup} ParameterKey=MyIamInstanceProfiles,ParameterValue=${MyIamInstanceProfiles} "
+PARAMS="${PARAMS} ParameterKey=AWSAMIID,ParameterValue=${AWSAMIID} ParameterKey=EC2InstanceType,ParameterValue=${EC2InstanceType} "
+PARAMS="${PARAMS} ParameterKey=MySSHKeyName,ParameterValue=${MySSHKeyName} "
+
+mkdir -p ~/.aws
+echo "aws ec2 delete-key-pair --region ${AWSRegion} --profile \${AWSprofile} --key-name ${MySSHKeyName} " > ${CFNfile}.sh
+echo "aws ec2 create-key-pair --region ${AWSRegion} --profile \${AWSprofile} --key-name ${MySSHKeyName} > ~/.aws/${MySSHKeyName}" >> ${CFNfile}.sh
+echo "aws cloudformation create-stack --stack-name ${MyVPCStackPrefix}-${JobSetName}-EC2-${MyEC2InstanceName}${ITEMNUMBER}  --region ${AWSRegion} --profile \${AWSprofile}  \
+		--parameters ${PARAMS} --template-body file://${CFNfile} " \
+		>> ${CFNfile}.sh
+			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! The STACK-NAME expression is VERY different vs. VPC and SG/EC2.. .. 
+			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! Especially, since EC2 instances have 'MyEC2InstanceName' added to Stack-name!
+			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! Make sure the STACK-NAME expression _OVERALL_ matching-up with 'MyStackNamePrefix' (see defn in cfngen-common.sh)
 
 ###------------------------------
 
