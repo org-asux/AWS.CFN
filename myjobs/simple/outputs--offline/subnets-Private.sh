55a94e09981b80d420340a9b8143b89c5320b1ba
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-07-12 19:13:42 -0400

    1st time in.  Only difference between the online and offline TEST-OUTPUT is one entry for HostedZoneId.  yeah.

---
+++ myjobs/simple/outputs--offline/subnets-Private.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Tokyo-subnets-Private-simple  --region ap-northeast-1 --profile ${AWSprofile} --template-body file:///tmp/subnets-Private.yaml
---
+++ myjobs/simple/outputs--offline/subnets-Public.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Tokyo-subnets-Public-simple  --region ap-northeast-1 --profile ${AWSprofile} --template-body file:///tmp/subnets-Public.yaml
