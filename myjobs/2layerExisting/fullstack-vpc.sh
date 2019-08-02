c55b26d05521260963ae5ce8278aec351c005526
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-08-02 19:08:19 -0400

    This is a true and final TEST output, that assumes a VPC and SG exist

--- myjobs/2layerExisting/fullstack-vpc.sh
+++
@@ -1,1 +0,0 @@
-aws cloudformation create-stack --stack-name org-ASUX-Playground-Canada-VPC  --region ca-central-1 --profile ${AWSprofile} --parameters ParameterKey=MyVPCStackPrefix,ParameterValue=org-ASUX-Playground-Canada --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/myjobs/2layerExisting/fullstack-vpc.yaml
