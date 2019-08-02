93f396838aa7c6324b1e695568096b3f89099f1a
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-08-02 19:07:20 -0400

    This is a true and final TEST output, that assumes a VPC and SG exist

--- myjobs/2layerExisting/fullstack-subnets-Private.sh
+++ myjobs/2layerExisting/fullstack-subnets-Private.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name org-ASUX-Playground-Canada-subnets-Private-2layerExisting  --region ca-central-1 --profile ${AWSprofile} --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/myjobs/2layerExisting/fullstack-subnets-Private.yaml
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Sydney-subnets-Private-2layerExisting  --region ap-southeast-2 --profile ${AWSprofile} --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/myjobs/2layerExisting/fullstack-subnets-Private.yaml
