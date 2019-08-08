54cc58a05a63e2b878b1cda77366a64f62e3597c
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-08-08 11:59:44 -0400

    Not tested. Now the cmdline is generated by CreateStackCmd.java and Not by simple String-definition

--- myjobs/2layer/fullstack-subnets-Public.sh
+++ myjobs/2layer/fullstack-subnets-Public.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name org-ASUX-Playground-Ohio-subnets-Public-2layer  --region us-east-2 --profile ${AWSprofile} --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/myjobs/2layer/fullstack-subnets-Public.yaml
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Ohio-subnets-Public-2layer --region us-east-2 --parameters  --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/myjobs/2layer/fullstack-subnets-Public.yaml --profile ${AWSprofile} 
