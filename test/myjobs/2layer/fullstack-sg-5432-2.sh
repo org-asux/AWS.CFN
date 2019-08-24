30c9092c9be1f08a5d8326476d00234434d827c2
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-08-23 23:46:11 -0400

    1st time in. Now SGs are auto-associated with EC2-instances for --fullstack-gen

---
+++ test/myjobs/2layer/fullstack-sg-5432-2.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Ohio-5432-2layer-2-SG --region us-east-2 --parameters  ParameterKey=MyVPC,ParameterValue=org-ASUX-Playground-Ohio-VPCID --template-body file:///Users/Sarma/Documents/Development/src/org.ASUX/AWS/CFN/test/myjobs/2layer/fullstack-sg-5432-2.yaml --profile ${AWSprofile} 
