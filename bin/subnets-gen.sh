9ebbc8aee0cef3ba8c07b7766797642a4b639d78
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-08-30 14:50:28 -0400

    New Public+NATGW command supported; Also, New 'Scope' property replaces 'PublicOrPrivate'

--- bin/subnets-gen.sh
+++ bin/subnets-gen.sh
@@ -26,8 +26,8 @@ CFNContext=subnets
 
 if [ "${VERBOSE}" == "1" ]; then echo "In subnets-gen.sh 3rd command line-arg = $2"; fi
 
-if [ $# -ge 2 ] && [[ "$2" == "public"  || "$2" == "private" ]]; then
-	PublicOrPrivate=$2
+if [ $# -ge 2 ] && [[ "$2" == "public"  || "$2" == "private" || "$2" == "public+natgw" ]]; then
+	Scope=$2
 else
 	echo "Usage: ${CmdPathGuess} $1 $2 <public|private>"
 	exit 4
@@ -39,8 +39,8 @@ fi
 
 CFNfile=/tmp/${CFNContext}
 
-${ORGASUXHOME}/asux.js yaml batch @${AWSCFNHOME}/bin/AWSCFN-${CFNContext}-${PublicOrPrivate}-Create.ASUX-batch.txt -i /dev/null -o ${CFNfile}
-echo "aws cloudformation create-stack --stack-name ${MyVPCStackPrefix}-subnets-${PublicOrPrivate}-${JobSetName}  --region ${AWSRegion} --profile \${AWSprofile} --template-body file://${CFNfile} " > ${CFNfile}.sh
+${ORGASUXHOME}/asux.js yaml batch @${AWSCFNHOME}/bin/AWSCFN-${CFNContext}-${Scope}-Create.ASUX-batch.txt -i /dev/null -o ${CFNfile}
+echo "aws cloudformation create-stack --stack-name ${MyVPCStackPrefix}-subnets-${Scope}-${JobSetName}  --region ${AWSRegion} --profile \${AWSprofile} --template-body file://${CFNfile} " > ${CFNfile}.sh
 			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! The STACK-NAME for VPC already has {ENVIRONMENT} in it.
 			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! DO ___NOT___ ADD {JobSetName} to Stack-name
 			#### !!!!!!!!!!!!!!!!!!!!!!!! ATTENTION !!!!!!!!!!!!!!!!!!!!!!!!!! The STACK-NAME expression is VERY different between VPC -vs- SG / EC2.. .. 
