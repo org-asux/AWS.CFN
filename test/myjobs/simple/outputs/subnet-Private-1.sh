1671f3011cc68ce0d0f4c93b78235b9dee1add64
Uday Bhaskar Sarma Seetamraju <ToSarma@gmail.com>
2019-09-04 14:52:30 -0400

    [CLEANUP] stackname has one uppercase Char

--- test/myjobs/simple/outputs/subnet-Private-1.sh
+++ test/myjobs/simple/outputs/subnet-Private-1.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name org-ASUX-Playground-Tokyo-Private-simple-1-subnet --region ap-northeast-1 --parameters  --template-body file:///tmp/subnet-Private-1.yaml --profile ${AWSprofile} 
+aws cloudformation create-stack --stack-name org-ASUX-Playground-Tokyo-Private-simple-1-Subnet --region ap-northeast-1 --parameters  --template-body file:///tmp/subnet-Private-1.yaml --profile ${AWSprofile} 
